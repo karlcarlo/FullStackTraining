12b97ad077ee46f984aa942664e41f3e5a85b82a
Karlcarlo <karlcarlo@163.com>
2015-11-26 11:01:55 +0800

    add sample config

---
+++ config/env/development.sample.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  db: {
+    uri: 'mongodb://localhost/fswf-dev',
+    options: {
+      user: '',
+      pass: ''
+    }
+  },
+  app: {
+      name: "Full Stack Web Framework - Development"
+  },
+  markdown: {
+    taskList: true
+  }
+}
